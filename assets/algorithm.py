da9b69029385fd0c03981da16abbaee3c11ae7f2
paranoiaz <79029644+paranoiaz@users.noreply.github.com>
2021-07-02 22:25:13 +0200

    minor refactor

--- assets/algorithm.py
+++ assets/algorithm.py
@@ -1,26 +1,24 @@
 import math
 import random
-
-from board import Board
-from token import Token
+from assets.board import Board
+from assets.token import Token
 
 
 class Algorithm:
 
     def __init__(self, difficulty: str):
-        self.difficulty = difficulty
+        self.difficulty = difficulty.upper()
 
-    def next_move(self, board: Board):
+    def execute_next_move(self, board: Board):
         if self.difficulty == "EASY":
             self.calculate_easy_move(board)
         if self.difficulty == "HARD":
             self.calculate_hard_move(board)
 
     def calculate_easy_move(self, board: Board):
-        random_number = random.randint(1, 9)
-
-        if board.is_position_empty(random_number - 1):
-            board.place_move(random_number - 1, Token.TOKEN_O)
+        random_number = random.randint(0, 8)
+        if board.is_position_empty(random_number):
+            board.place_move(random_number, Token.TOKEN_O)
             return
 
         self.calculate_easy_move(board)
@@ -29,36 +27,35 @@ class Algorithm:
         best_move = 0
         best_score = -math.inf
 
-        for move in range(1, 10):
-            if board.is_position_empty(move - 1):
-                board.place_move(move - 1, Token.TOKEN_O)
+        for move in range(0, 9):
+            if board.is_position_empty(move):
+                board.place_move(move, Token.TOKEN_O)
                 score = self.run_mini_max(board, False)
-                board.place_move(move - 1, Token.TOKEN_EMPTY)
+                board.place_move(move, Token.TOKEN_EMPTY)
 
                 if score > best_score:
                     best_score = score
                     best_move = move
 
-        board.place_move(best_move - 1, Token.TOKEN_O)
+        board.place_move(best_move, Token.TOKEN_O)
 
-    def run_mini_max(self, board: Board, is_maximizing):
+    def run_mini_max(self, board: Board, is_maximizing: bool):
         if board.check_winner() == Token.TOKEN_O:
             return 1
-
         if board.check_winner() == Token.TOKEN_X:
             return -1
-
         if board.check_full_board():
             return 0
 
         if is_maximizing:
             best_score = -math.inf
 
-            for move in range(1, 10):
-                if board.is_position_empty(move - 1):
-                    board.place_move(move - 1, Token.TOKEN_O)
+            for move in range(0, 9):
+                if board.is_position_empty(move):
+                    # bot token
+                    board.place_move(move, Token.TOKEN_O)
                     score = self.run_mini_max(board, False)
-                    board.place_move(move - 1, Token.TOKEN_EMPTY)
+                    board.place_move(move, Token.TOKEN_EMPTY)
 
                     if score > best_score:
                         best_score = score
@@ -66,15 +63,16 @@ class Algorithm:
             return best_score
 
         else:
-            best_score = math.inf
+            worst_score = math.inf
 
-            for move in range(1, 10):
-                if board.is_position_empty(move - 1):
-                    board.place_move(move - 1, Token.TOKEN_X)
+            for move in range(0, 9):
+                if board.is_position_empty(move):
+                    # player token
+                    board.place_move(move, Token.TOKEN_X)
                     score = self.run_mini_max(board, True)
-                    board.place_move(move - 1, Token.TOKEN_EMPTY)
+                    board.place_move(move, Token.TOKEN_EMPTY)
 
-                    if score < best_score:
-                        best_score = score
+                    if score < worst_score:
+                        worst_score = score
 
-            return best_score
+            return worst_score
